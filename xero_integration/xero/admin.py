5a5ee611d7f1ea85e13834fa44c6ebb6090338c4
VicGanoh <kofiganoh@gmail.com>
2025-02-12 17:27:09 +0000

    add model admins

--- xero_integration/xero/admin.py
+++ xero_integration/xero/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Contact, ContactAddress, ContactPhoneNumber, ContactPerson
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    search_fields = ["name", "email"]
+    list_display = ["name", "email", "is_supplier", "is_customer"]
+    list_filter = ["is_supplier", "is_customer"]
+
+@admin.register(ContactAddress)
+class ContactAddressAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["company_name"]
+    list_display = ["company_name", "address_type", "city", "country"]
+    list_filter = ["address_type", "country"]
+    search_fields = ["company_name__name", "city", "address_line1"]
+
+@admin.register(ContactPhoneNumber)
+class ContactPhoneNumberAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["company_name"]
+    list_display = ["company_name", "phone_label", "phone_number"]
+    list_filter = ["phone_label"]
+    search_fields = ["company_name__name", "phone_number"]
+
+@admin.register(ContactPerson)
+class ContactPersonAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["company_name"]
+    list_display = ["full_name", "company_name", "job_title", "email", "primary_contact"]
+    list_filter = ["primary_contact"]
+    search_fields = ["first_name", "last_name", "company_name__name", "email"]
